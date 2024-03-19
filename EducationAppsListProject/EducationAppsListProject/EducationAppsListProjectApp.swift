21c91b6ffc611315d1030d77fea9216d02378258
Takuue <139447901+Takues@users.noreply.github.com>
2024-03-19 11:55:36 +0200

    Add files via upload

---
+++ EducationAppsListProject/EducationAppsListProject/EducationAppsListProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationAppsListProjectApp.swift
+//  EducationAppsListProject
+//
+//  Created by Takudzwa Zindoga on 19/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct EducationAppsListProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
